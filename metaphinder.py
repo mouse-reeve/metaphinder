d27c98eb604c0f3d7f8b05e22ff6ee2cf65ff132
Mouse Reeve <mousereeve@riseup.net>
2017-12-08 13:44:38 -0800

    Formulate a comparison

--- metaphinder.py
+++ metaphinder.py
@@ -3,17 +3,18 @@ import blacklist
 from datetime import datetime
 import re
 import settings
+import sys
 from TwitterAPI import TwitterAPI
 
 API = TwitterAPI(settings.API_KEY, settings.API_SECRET,
                  settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
 
-def build_metaphor():
+def build_metaphor(adjective):
     ''' create a comparison sentence based on tweets '''
     # pick an adjective around which to build the comparison
-    adjective = 'green'
     prompt = 'is %s' % adjective
 
+    nouns = []
     # find structurally compatible tweets that use the adjective
     tweets = API.request('search/tweets', {'q': '"%s"' % prompt})
     for tweet in tweets:
@@ -26,15 +27,25 @@ def build_metaphor():
             continue
 
         # detect a "my/the __ is green" pattern
-        if re.match(r'(my|the) ([.*]{1,20}) %s' % prompt, text):
-            print text
-            import pdb;pdb.set_trace()
-        import pdb;pdb.set_trace()
+        print(text)
+        articles = '|'.join(['my', 'his', 'her', 'a', 'an', 'the', 'that'])
+        match = re.search(r'(\b%s\b) (.{1,20}) %s' % \
+                (articles, prompt), text.lower())
+        if match and len(match.groups()) > 1:
+            nouns.append(match.groups())
+
+        if len(nouns) == 2:
+            break
+    print(nouns)
 
     # build the sentence
-    text = ''
+    if len(nouns) >= 2:
+        result = '%s %s is as %s as %s %s' % \
+            (nouns[0][0], nouns[0][1], adjective, nouns[1][0], nouns[1][1])
+        print result
+        return result
 
-    return text
+    return False
 
 
 def post_tweet():
@@ -42,7 +53,7 @@ def post_tweet():
     print('--------- creating metaphor --------')
     print(datetime.today().isoformat())
 
-    text = build_metaphor()
+    text = build_metaphor('green')
     print(text)
 
     print('--------- posting tweet --------')
@@ -51,4 +62,9 @@ def post_tweet():
 
 
 if __name__ == '__main__':
-    post_tweet()
+    #post_tweet()
+    try:
+        input_adjective = sys.argv[1]
+    except IndexError:
+        input_adjective = 'green'
+    build_metaphor(input_adjective)
