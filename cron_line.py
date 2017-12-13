8ac9ceae308f0757ebffcce2e173b66c3f81040d
Mouse Reeve <mousereeve@riseup.net>
2017-12-12 23:16:32 -0800

    we queuing

--- cron_line.py
+++ cron_line.py
@@ -2,9 +2,8 @@
 from bot import settings
 
 # populate queue
-if settings.USE_QUEUE:
-    print('5 */%s * * * %s/venv/bin/python3 %s/bot/generator.py' % \
-            (settings.FREQUENCY, settings.FILEPATH, settings.FILEPATH))
+print('5 */%s * * * %s/venv/bin/python3 %s/bot/generator.py' % \
+        (settings.FREQUENCY, settings.FILEPATH, settings.FILEPATH))
 
 # actual tweeting
 print('0 */%s * * * %s/venv/bin/python3 %s/bot/tweet.py' % \
